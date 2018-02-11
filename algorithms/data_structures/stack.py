1f204711eaea42d91886335914135fc5c98337c3
Nathan Patnam <patnamnv@email.wofford.edu>
2018-02-11 07:10:34 -0500

    Fixed Stack Class
    - Added operations to heading
    - Fixed operation names ie add -> push
    - will update unit tests to reflect changes

--- algorithms/data_structures/stack.py
+++ algorithms/data_structures/stack.py
@@ -6,6 +6,14 @@
     adds an element to the collection, and pop, which removes the last element
     that was added.
 
+    Operations:
+    is_empty()
+    peek()
+    pop()
+    push()
+    size()
+     
+
     Pseudo Code: https://en.wikipedia.org/wiki/Stack_%28abstract_data_type%29
 """
 
@@ -14,36 +22,53 @@ class Stack:
 
     def __init__(self):
         self.stack_list = []
+        self.size = 0
 
-    def add(self, value):
+
+    def is_empty(self):
         """
-        Add element at last
+        Return True if stack is empty 
+
+        Time Complexity:  O(1)
+        """
+        return self.size == 0
+    
+     def peek(self):
+        """
+        Return the item currently on top of the stack
+
+        Time Complexity:  O(1)
+        """
+        return self.stack_list[self.size-1]
+     
+
+    def pop(self):
+        """
+        Remove and return element on top of stack
+
+        Time Complexity:  O(1)
+
+        Error will be thrown if size is 0
+        """
+        self.size -= 1 
+        return self.stack_list.pop()
+
+    def push(self, value):
+        """
+        Add element to top of stack
 
         Time Complexity:  O(1)
         """
         self.stack_list.append(value)
+        self.size += 1
 
-    def remove(self):
-        """
-        Remove element from last return value
-
-        Time Complexity:  O(1)
-        """
-
-        return self.stack_list.pop()
-
-    def is_empty(self):
-        """
-        1 value returned on empty 0 value returned on not empty
-
-        Time Complexity:  O(1)
-        """
-        return not self.size()
-
+  
     def size(self):
         """
         Return size of stack
 
         Time Complexity:  O(1)
         """
-        return len(self.stack_list)
+        return self.size
+    
+   
